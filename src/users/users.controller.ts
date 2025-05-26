7a3013e722404ee5305910b7f0a2ec9691c00253
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-25 23:00:18 -0400

    feat: implement CRUD operations in UsersController
    - Added methods for handling user data:
      - findAll: Retrieve all users or filter by role
      - findOne: Get a user by ID
      - create: Add a new user
      - update: Modify an existing user
      - delete: Remove a user by ID

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // GET /users or /users?role=value
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return { role: role || 'all' };
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /users
+  @Post()
+  create(@Body() user: Record<string, string>) {
+    return user;
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: Record<string, string>) {
+    return {
+      id,
+      ...userUpdate,
+    };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return id;
+  }
+}
